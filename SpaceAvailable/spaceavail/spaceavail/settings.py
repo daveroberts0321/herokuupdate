e2acc9701ffc0567e73a1cab4cf00d62b6e3c5fd
Dave Roberts <dave@tac-llc.com>
2019-07-07 13:38:40 -0500

    20190707.1

--- SpaceAvailable/spaceavail/spaceavail/settings.py
+++ SpaceAvailable/spaceavail/spaceavail/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'spaceavail.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'udslcnqb',
+        'USER': 'udslcnqb',
+        'PASSWORD': '9-Nu2f-0uatzPgpehqTIQv4ZoKo8zZIm',
+        'HOST': 'raja.db.elephantsql.com',
+        'PORT': '5432',
+
     }
 }
 
@@ -119,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
--- SpaceAvailable/spaceavail/spaceavail/urls.py
+++ SpaceAvailable/spaceavail/spaceavail/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
+from loads import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('', include('loads.urls')),
 ]
