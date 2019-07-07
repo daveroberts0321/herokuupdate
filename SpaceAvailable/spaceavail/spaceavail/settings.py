31953e0b283e9ee0e81c6b08884e3f522a0ca841
Dave Roberts <dave@tac-llc.com>
2019-07-07 14:53:31 -0500

    20190707.8 heroku update

--- SpaceAvailable/spaceavail/spaceavail/settings.py
+++ SpaceAvailable/spaceavail/spaceavail/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import django_heroku
+import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,5 +128,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-# Activate Django-Heroku.
+# Configure Django App for Heroku.
+import django_heroku
 django_heroku.settings(locals())
